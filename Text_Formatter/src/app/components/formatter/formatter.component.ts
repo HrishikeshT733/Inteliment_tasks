3e9f26a7f66c294fc7f1854d48900a914c7f7fd8
HrishikeshT733 <hrishikeshtholbare@gmail.com>
2026-04-06 16:16:48 +0530

    Use custom pipe to remove special character from input text

--- Text_Formatter/src/app/components/formatter/formatter.component.ts
+++ Text_Formatter/src/app/components/formatter/formatter.component.ts
@@ -1,105 +1,3 @@
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
-// import { TextService } from '../../services/text.service';
-// import { FormattingService } from '../../services/formatting.service';
-// import { Subscription } from 'rxjs';
-
-// @Component({
-//   selector: 'app-formatters',
-//   standalone: true,
-//   imports: [CommonModule, FormsModule],
-//   templateUrl: './formatter.component.html',
-//   styleUrls: ['./formatter.component.css']
-// })
-// export class FormattersComponent {
-//   currentText: string = '';
-//   selectedColor: string = '#000000';
-//   charCount:number=0;
-//   wordCount:number=0;
-//     private subscriptions: Subscription = new Subscription();
-//   ngOnInit():void {
-//      this.subscriptions.add(this.textService.charCount$.subscribe(count=>{
-//       this.charCount=count;
-//      }));
-
-//      this.subscriptions.add(this.textService.wordCount$.subscribe(count=>{
-//       this.wordCount=count;
-//      }));
-//   }
-//   constructor(
-//     private textService: TextService,
-//     private formattingService: FormattingService
-//   ) {
-//     this.textService.text$.subscribe(text => {
-//       this.currentText = text;
-//     });
-//   }
-
-//   // Text formatting functions
-//   removeWhiteSpace(): void {
-//     const formattedText = this.currentText.replace(/\s+/g, ' ').trim();
-//     this.textService.updateOutputText(formattedText);
-//   }
-
-//   reverseAll(): void {
-//     const reversedText = this.currentText.split(' ').reverse().join(' ');
-//     this.textService.updateOutputText(reversedText);
-//   }
-
-//   removeSpecialChars(): void {
-//     const cleanedText = this.currentText.replace(/[^a-zA-Z0-9\s]/g, '');
-//     this.textService.updateOutputText(cleanedText);
-//   }
-
-//   capitalizeWord(): void {
-//     const capitalizedText = this.currentText.toUpperCase();
-//     this.textService.updateOutputText(capitalizedText);
-//   }
-//   //  capitalizeWord(): void {
-//   //   const capitalizedText = this.currentText.replace(/\b\w/g, char => char.toUpperCase());
-//   //   this.textService.updateText(capitalizedText);
-//   // }
-
-
-//    clearAll(): void {updateOutputText
-    
-//     this.textService.('');
-//   }
-
-//   // Styling functions
-//   toggleBold(): void {
-//     this.formattingService.toggleBold();
-//   }
-
-//   toggleItalic(): void {
-//     this.formattingService.toggleItalic();
-//   }
-
-//   toggleUnderline(): void {
-//     this.formattingService.toggleUnderline();
-//   }
-
-//   changeColor(color: string): void {
-//     this.selectedColor = color;
-//     this.formattingService.changeColor(color);
-//   }
-
-//   increaseFontSize(): void {
-//     this.formattingService.increaseFontSize();
-//   }
-
-//   decreaseFontSize(): void {
-//     this.formattingService.decreaseFontSize();
-//   }
-
-//   removeStyling(): void {
-//     this.formattingService.removeStyling();
-//   }
-// }
-
-
-
 
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -107,13 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { TextService } from '../../services/text.service';
 import { FormattingService } from '../../services/formatting.service';
 import { Subscription } from 'rxjs';
-
+import { RemoveSpecialCharPipe } from '../../pipes/remove-special-char.pipe';
 @Component({
   selector: 'app-formatters',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './formatter.component.html',
-  styleUrls: ['./formatter.component.css']
+  styleUrls: ['./formatter.component.css'],
+  providers:[RemoveSpecialCharPipe]
 })
 export class FormattersComponent implements OnInit, OnDestroy {
   currentText: string = '';
@@ -123,6 +22,7 @@ export class FormattersComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription = new Subscription();
 
   constructor(
+    private RemoveSpecialCharPipe:RemoveSpecialCharPipe,
     private textService: TextService,
     private formattingService: FormattingService
   ) {}
@@ -161,7 +61,7 @@ export class FormattersComponent implements OnInit, OnDestroy {
   }
 
   removeSpecialChars(): void {
-    const cleanedText = this.currentText.replace(/[^a-zA-Z0-9\s]/g, '');
+    const cleanedText = this.RemoveSpecialCharPipe.transform(this.currentText);
     this.textService.updateOutputText(cleanedText);
   }
 
--- Text_Formatter/src/app/components/navbar/navbar.component.ts
+++ Text_Formatter/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink,RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
